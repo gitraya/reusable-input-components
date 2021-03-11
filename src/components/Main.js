fd91f12cccd6e2cd466a4fe28ed2bc9c797c8e33
gitraya <mail.raya15@gmail.com>
2021-03-11 17:05:50 +0700

    Upload main files

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import Input from './Input/Input';
+
+const Main = (text) => {
+  return (
+    <main>
+      <h2 className="ft-500 ft-poppins cl-gytwo">Inputs</h2>
+    </main>
+  );
+};
+
+export default Main;
